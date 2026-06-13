77e2468bca1f6f600e886be949dea6879bf691f4
Rustik <chuckis94@gmail.com>
2026-06-13 18:37:06 +0300

    fix: prismjs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { I18nProvider } from "./i18n/I18nProvider";
 import { ThemeProvider } from "./theme/ThemeProvider";
 import "./index.css";
+import "prismjs";
+import "prismjs/themes/prism.css";
 
 function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
